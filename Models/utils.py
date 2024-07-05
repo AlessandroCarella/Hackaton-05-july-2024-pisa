7d6528a636534da064cfc996b21a2fd53da109f3
Sara Hoxha <sarahoxha99@gmail.com>
2024-07-05 12:59:04 +0200

    added function for utils

--- Models/utils.py
+++ Models/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def get_X_train ():
     return pd.read_csv('../Dataset/X_train.csv')
@@ -20,4 +21,13 @@ def get_All ():
     Y_train = get_Y_train() 
     Y_test = get_Y_test()
 
+    return X_train, X_test, Y_train, Y_test
+
+
+def get_Datasets_With_25_Split():
+    df=pd.read_csv('../Dataset/datasetWithTarget.csv')
+    train_cols = df.columns.difference(['customer_types', 'conversion', 'offer'])
+    y = df[train_cols] 
+    X = df['customer_types']
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.25, random_state=12)
     return X_train, X_test, Y_train, Y_test
