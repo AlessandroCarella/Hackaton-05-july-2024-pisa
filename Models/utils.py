80dc73b644eec7468bb493bb55f3d3e42ad6db24
Sara Hoxha <sarahoxha99@gmail.com>
2024-07-05 11:41:23 +0200

    pushing utils file

---
+++ Models/utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def get_X_train ():
+    return pd.read_csv('../Dataset/X_train.csv')
+
+def get_X_test ():
+    return pd.read_csv('../Dataset/X_test.csv')
+
+def get_Y_train ():
+    return pd.read_csv('../Dataset/Y_train.csv')
+
+def get_Y_test ():
+    return pd.read_csv('../Dataset/Y_test.csv')
+
+
+def get_All ():
+    
+    X_train = get_X_train()
+    X_test = get_X_test()
+    Y_train = get_Y_train() 
+    Y_test = get_Y_test()
+
+    return X_train, X_test, Y_train, Y_test
