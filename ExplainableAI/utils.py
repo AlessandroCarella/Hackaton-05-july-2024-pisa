62a997bfa1e3173d334ccd8d5474702b97b1c933
Alessandro Carella <alessandro.carella.universita@gmail.com>
2024-07-05 15:48:54 +0200

    changed method name

--- ExplainableAI/utils.py
+++ ExplainableAI/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def get_Datasets_With_25_Split():
+def getDataset():
     df=pd.read_csv('../Dataset/final dataset.csv')
 
     train_cols = df.columns.difference(['customer_types', 'conversion', 'offer', 'treat'])
