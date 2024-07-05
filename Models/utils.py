1fb8be82f3503ca8e7ddecd045cf1f691955439f
Sara Hoxha <sarahoxha99@gmail.com>
2024-07-05 14:47:17 +0200

    changed name of file

--- Models/utils.py
+++ Models/utils.py
@@ -25,11 +25,36 @@ def get_All ():
 
 
 def get_Datasets_With_25_Split():
-    df=pd.read_csv('../Dataset/resampled_dataset.csv')
+    df=pd.read_csv('../Dataset/resampled_dataset cluster centroids.csv')
 
-    train_cols = df.columns.difference(['customer_types', 'conversion'])
+    train_cols = df.columns.difference(['customer_types', 'conversion', 'offer'])
     X = df[train_cols] 
-    y = df['customer_types']
+    y = df['treat']
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.25, random_state=12)
     return X_train, X_test, Y_train, Y_test
+
+
+def target_ova(df):
+    #FIRST SPLIT (ONE VS ALL)
+    #0(do not treat) -> 0,1,2
+    #1(treat) -> 3
+
+    """Declare target class
+    """
+    #Lost Causes:
+    df.loc[df['customer_types'].isin([0, 1, 2]), 'treat'] = 0
+    df.loc[df['customer_types'] == 3, 'treat'] = 1
+    return df
+
+
+def target_2(df):
+    #SECOND SPLIT (TWO VS TWO)
+    #0(do not treat) -> 0,1
+    #1( treat) -> 2,3
+    """Declare target class
+    """
+    #Lost Causes:
+    df.loc[df['customer_types'].isin([0, 1]), 'treat'] = 0
+    df.loc[df['customer_types'].isin([2,3]), 'treat'] = 1
+    return df
